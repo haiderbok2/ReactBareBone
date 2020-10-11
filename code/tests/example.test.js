19fd4a8c3284f1df6942c472fff8b8c9f327a17b
bokharim <bokharim@purdue.edu>
2020-10-11 01:45:00 -0400

    added snapshot test, configured global variables for jest

--- code/tests/example.test.js
+++ code/tests/example.test.js
@@ -1,26 +1,51 @@
 const puppeteer = require('puppeteer');
+const ScreenshotTester = require('puppeteer-screenshot-tester')
 const assert = require('assert');
 
-// (async () => {
-//   const browser = await puppeteer.launch({headless: false});
-//   const page = await browser.newPage();
-//   await page.goto('http://localhost:3000/');
-//   await page.screenshot({path: 'example.png'});
-//   assert.strictEqual(page.url(), 'http://localhost:3000/home' , 'test checking signin page url failed').catch((error) => {
 
-//   });
-//   await browser.close();
-// })();
+beforeAll(async () => {
+   global.browser = await puppeteer.launch();
+   global.page = await browser.newPage();
+   global.tester = await ScreenshotTester(0.8, false, false, [], {});
+});
 
+/* test to check if the url takes us to the sign in page */
 test("test sign in page url", async () => {
-  
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000/');
-  const url = await page.url();
-  
 
+  await global.page.goto('http://localhost:3000/');
+  const url = await global.page.url();
+  
+  await page.screenshot({path : 'signIn-screen.png'});
   assert(url === 'http://localhost:3000/')
-  await browser.close();
 
 })
+
+/* Snapshot test for home screen */
+
+test ("Snapshot testing of sign in page ", async () => {
+  const result = await global.tester(global.page, "test1", {
+    fullPage: true,
+  });
+
+  expect(result).toBe(true);
+});
+
+/* test to check if if hitting the submit button takes us to the home page */
+
+test ("testing submit button routing to home page", async () => {
+  
+  const submit =  await page.$("#root > div > div > a");
+  await submit.evaluate(submit => submit.click());
+  const url = await global.page.url();
+  await page.screenshot({path : 'home-screen.png'});
+  assert(url === 'http://localhost:3000/home');
+
+})
+
+
+afterAll(async () => {
+  await global.browser.close();
+});
+
+
+
